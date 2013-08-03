d4a694dab5e4f29ebdc8afe7708bbb478ac20b43
SAMOXA <amdsamoxa@gmail.com>
2013-08-03 11:02:43 +0600

    Fix git again

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <QtGui/QGuiApplication>
-<<<<<<< HEAD
 #include <QtQml>
 #include "qtquick2applicationviewer.h"
 #include "vkagent.h"
-=======
-#include "qtquick2applicationviewer.h"
->>>>>>> c951ebba88108b8a93b0fd044f6735f884ab6e74
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-<<<<<<< HEAD
     //QApplication::setApplicationName("VkSaver");
     //QApplication::setApplicationVersion("0.01a");
     VkAgent agent;
@@ -21,12 +16,9 @@ int main(int argc, char *argv[])
     viewer.setMainQmlFile(QStringLiteral("qml/VkSaver/main.qml"));
 
     viewer.showExpanded();
-=======
 
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/VkSaver/main.qml"));
     viewer.showExpanded();
-
->>>>>>> c951ebba88108b8a93b0fd044f6735f884ab6e74
     return app.exec();
 }
